b042c813d2d59fc97c8a4636ef47b972355ea48b
JavierHernandez7-bit <hm210444@alumno.udb.edu.sv>
2021-04-25 23:02:16 -0600

    subiendo guia 11

---
+++ ejercicio2Guia11/ejercicio2Guia11/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Guia11_Ej1
+{
+    class Program
+    {
+
+        unsafe static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow; Console.BackgroundColor = ConsoleColor.Black; Console.Clear();
+            int[] vect = new int[10];
+            int* ap;
+            int pc = 0;
+            for (int x = 0; x < 10; x++)
+            {
+                Console.Write($"Digite el dato {x + 1}: ");
+                vect[x] = int.Parse(Console.ReadLine());
+            }
+            
+            Console.WriteLine("Introduzca la matriz:");
+            for (int x = 0; x < 10; x++)
+            {
+                pc = vect[x];
+                ap = &pc;
+                Console.WriteLine($"Numero [{x}]: {*ap}");
+            }
+            Console.WriteLine("Para ver la matriz ordenada, por favor, presione cualquier tecla");
+            Console.ReadKey();
+            
+            for (int k = 0; k < 9; k++)
+            {
+                for (int f = 0; f < 9 - k; f++)
+                {
+                    if (vect[f] > vect[f + 1])
+                    {
+                        int pru;
+                        pru = vect[f];
+                        vect[f] = vect[f + 1];
+                        vect[f + 1] = pru;
+                    }
+                }
+            }
+            Console.WriteLine("Ordenado:");
+            for (int x = 0; x < 10; x++)
+            {
+                pc = vect[x];
+                ap = &pc;
+                Console.WriteLine($"Numero [{x + 1}]: {*ap}");
+            }
+            Console.WriteLine("Si desea salir, presiones cualquier tecla del teclado");
+            Console.ReadKey();
+        }
+    }
+}
